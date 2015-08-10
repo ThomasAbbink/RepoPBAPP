50c3145bcaee6830f09193ee252a95114d74ad2f
Arno <arnohuisman@gmail.com>
2015-08-10 23:16:58 +0200

    Programme list now activates a new Filmactivity. This Activity gets resources from strings.xml and loads them into TextViews. Also added a Chrome client inside a WebView which directly loads  youtube url. Fullscreen and/or scaling does not work. This was just a quick feature, a better option is to use the Youtube API, but this involves quite some setup.

---
+++ app/src/main/java/com/thomas/pleinbioscoop/FilmActivity.java
@@ -0,0 +1,68 @@
+package com.thomas.pleinbioscoop;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.content.res.Resources;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.webkit.WebSettings;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
+import android.widget.TextView;
+
+
+public class FilmActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_film);
+
+        Intent intent = getIntent();
+        int index = intent.getIntExtra("index", 0);
+
+        Resources res = getResources();
+        String title = res.getStringArray(R.array.titles)[index];
+        String info = res.getStringArray(R.array.info)[index];
+        String plot = res.getStringArray(R.array.plot)[index];
+
+        ((TextView)findViewById(R.id.movietitle)).setText(title);
+        ((TextView)findViewById(R.id.movieinfo)).setText(info);
+        ((TextView)findViewById(R.id.movieplot)).setText(plot);
+
+        String frameVideo = "<html><body><iframe width=\"320\" height=\"315\" src=\"https://www.youtube.com/embed/lY2H2ZP56K4\" frameborder=\"0\" allowfullscreen></iframe></body></html>";
+        WebView displayVideo = (WebView)findViewById(R.id.movieWebView);
+        displayVideo.setWebViewClient(new WebViewClient(){
+            @Override
+            public boolean shouldOverrideUrlLoading(WebView view, String url) {
+                return false;
+            }
+        });
+        WebSettings webSettings = displayVideo.getSettings();
+        webSettings.setJavaScriptEnabled(true);
+        displayVideo.loadData(frameVideo, "text/html", "utf-8");
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_film, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+}
--- app/src/main/java/com/thomas/pleinbioscoop/ProgrammaFragment.java
+++ app/src/main/java/com/thomas/pleinbioscoop/ProgrammaFragment.java
@@ -2,6 +2,7 @@ package com.thomas.pleinbioscoop;
 
 import android.content.Context;
 import android.content.Intent;
+import android.content.res.Resources;
 import android.os.Bundle;
 import android.support.v4.app.ListFragment;
 import android.view.LayoutInflater;
@@ -9,6 +10,7 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
+import android.widget.TextView;
 import android.widget.Toast;
 
 public class ProgrammaFragment extends ListFragment {
@@ -18,25 +20,9 @@ public class ProgrammaFragment extends ListFragment {
     public void onActivityCreated(Bundle savedInstanceState) {
         super.onActivityCreated(savedInstanceState);
 
-        String[] movies = {"wo 12 aug – Whiplash",
-        "do 13 aug – Frank",
-        "vr 14 aug – The Imitation Game",
-        "za 15 aug – Chef",
-        "zo 16 aug–Tussen 10 en 12",
-        "ma 17 aug–Pride",
-        "di 18 aug–Sergio Herman:Fucking Perfect",
-        "wo 19 aug–White God",
-        "do 20 aug– Indiana Jones and the Temple of Doom",
-        "vr 21 aug – Guardians of the Galaxy",
-        "za 22 aug – Birdman",
-        "zo 23 aug – Meraviglie",
-        "ma 24 aug – Still Alice",
-        "di 25 aug – Turist",
-        "wo 26 aug – Foxcatcher",
-        "do 27 aug – Taxi Driver",
-        "vr 28 aug – Wild",
-        "za 29 aug – Gone Girl",
-        "zo 30 aug – Love is Strange"};
+        // Get the movies array from strings.xml
+        Resources res = getResources();
+        String[] movies = res.getStringArray(R.array.movies);
 
         // Populate list with our static array of titles.
         ArrayAdapter<String> adapter = new ArrayAdapter<String>(getActivity(),
@@ -50,7 +36,6 @@ public class ProgrammaFragment extends ListFragment {
 
     }
 
-
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
@@ -60,11 +45,13 @@ public class ProgrammaFragment extends ListFragment {
         return rootView;
     }
 
-
     @Override
     public void onListItemClick (ListView l, View v, int position, long id){
-        super.onListItemClick(l, v, position, id);
-
-
+        Intent myIntent = new Intent(getActivity(), FilmActivity.class);
+        myIntent.putExtra("index", position); //Optional parameters
+        String message = position+": "+((TextView)v).toString();
+        Toast.makeText(getActivity(), message, Toast.LENGTH_SHORT).show();
+        getActivity().startActivity(myIntent);
     }
+
 }
