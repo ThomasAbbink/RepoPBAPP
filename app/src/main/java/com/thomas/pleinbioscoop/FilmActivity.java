8dd4d1797995d30e02cf6549b1cca92c423abbab
Arno <arnohuisman@gmail.com>
2015-08-11 12:36:32 +0200

    Added gradient background in Film Activity

--- app/src/main/java/com/thomas/pleinbioscoop/FilmActivity.java
+++ app/src/main/java/com/thomas/pleinbioscoop/FilmActivity.java
@@ -31,17 +31,7 @@ public class FilmActivity extends Activity {
         ((TextView)findViewById(R.id.movieinfo)).setText(info);
         ((TextView)findViewById(R.id.movieplot)).setText(plot);
 
-        String frameVideo = "<html><body><iframe width=\"320\" height=\"315\" src=\"https://www.youtube.com/embed/lY2H2ZP56K4\" frameborder=\"0\" allowfullscreen></iframe></body></html>";
-        WebView displayVideo = (WebView)findViewById(R.id.movieWebView);
-        displayVideo.setWebViewClient(new WebViewClient(){
-            @Override
-            public boolean shouldOverrideUrlLoading(WebView view, String url) {
-                return false;
-            }
-        });
-        WebSettings webSettings = displayVideo.getSettings();
-        webSettings.setJavaScriptEnabled(true);
-        displayVideo.loadData(frameVideo, "text/html", "utf-8");
+
     }
 
     @Override
